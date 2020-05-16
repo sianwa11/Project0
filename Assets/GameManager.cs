d3ed721edcb69f1abe2731454611524d52d13a6b
sianwa11 <sianwa.atemi@strathmore.edu>
2020-05-16 21:41:45 +0300

    first commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    Boolean gameHasEnded = false;
+    public float restardDelay = 1f;
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("game over");
+            Invoke("Restart", restardDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player; // stores reference to player
+    public Vector3 offset; // allows us to offset position
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set our position to the players position and offset it
+        transform.position = player.position + offset;
+    }
+}
